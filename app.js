bc770ae57e600da4e1219c85aaba1af16d10aee8
AMIT VERMA <55926153+amitverma07@users.noreply.github.com>
2020-04-21 22:10:43 -0500

    Initial Build

---
+++ app.js
@@ -0,0 +1,16 @@
+var typed = new Typed('.element', {
+  strings: [
+    'I am a Full Stack Developer.', 
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React JS',
+    'C#',
+    'ASP.NET',
+    'MySQl',
+    'MongoDB',
+  ],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true,
+});
